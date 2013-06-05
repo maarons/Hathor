5beae82e59e3584e41b555d8173df6e5cedb61f8
Marek Sapota <marek@sapota.org>
2013-06-05 02:30:13 -0700

    Support for postgres

--- model/__init__.py
+++ model/__init__.py
@@ -3,7 +3,18 @@ from model.Episode import Episode
 from model.Season import Season
 from model.TvSeries import TvSeries
 from model.Provider import Provider
+from config import env
+import config
 
 from sqlalchemy import create_engine
-engine = create_engine("sqlite:///db.sqlite3", echo = False)
+if env == "development":
+    engine = create_engine("sqlite:///db.sqlite3", echo = False)
+elif env == "production":
+    engine = create_engine("postgresql://{}:{}@localhost/{}".format(
+        config.pg_user,
+        config.pg_pass,
+        config.pg_dbname,
+    ), client_encoding = "utf8")
+else:
+    raise Exception("Unknown environment: " + env)
 Base.metadata.create_all(engine)
